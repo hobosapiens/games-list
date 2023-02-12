0c80144a81c33f69c0b0a98224bb5290ee6fb5e9
Anatoly Popov <trudovoe.popov@gmail.com>
2023-02-12 14:00:14 +0100

    Cards stylizing

--- src/components/GamesPage/GameCard/GameCard.jsx
+++ src/components/GamesPage/GameCard/GameCard.jsx
@@ -8,13 +8,17 @@ import styles from './GameCard.module.scss';
 const GameCard = ({ game }) => {
     return (
         <li className={ styles.card }>
-            <img className={ styles.item__img } src={
-                `http://www.royalgames.com/images/games/${game.short}/${game.short}_170x80.gif`
-            } alt={game.name} />
+            <img 
+                className={ styles.card__img } 
+                src={`http://www.royalgames.com/images/games/${game.short}/${game.short}_170x80.gif`} 
+                alt={game.name} 
+            />
             <div className={styles.card__info}>
                 <div className={styles.card__favorite}><UiToggleFavorite game={game.short} /></div>
-                <Link className={ styles.card__genre } to={`/spel/${game.genre}`}>#{game.genre}</Link>
-                <Link to={game.url} className={ styles.card__name }>{game.name}</Link>
+                <div className={styles.card__text}>
+                    <Link className={ styles.card__genre } to={`/spel/${game.genre}`}>#{game.genre}</Link>
+                    <Link to={game.url} className={ styles.card__name }>{game.name}</Link>
+                </div>
             </div>
         </li>
     )
